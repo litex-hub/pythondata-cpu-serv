c8150e69365dbb18aeebe27594daad6a1754b558
LiteX <robot@timvideos.us>
2020-05-27 05:55:26 +0000

    Updating pythondata-cpu-serv to 1.0.post78
    Updated data to v1.0-25-gacbedbe based on acbedbe9c4bbff4bb0d2cdda725a95f2e1c42adc from https://github.com/olofk/serv.
    > commit acbedbe9c4bbff4bb0d2cdda725a95f2e1c42adc
    > Author: Olof Kindgren <olof.kindgren@gmail.com>
    > Date:   Tue May 26 22:52:09 2020 +0200
    >
    >     Prepare for release
    >
    
    Updated using 0.0.post53 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.0.post74"
-version_tuple = (1, 0, 74)
+version_str = "1.0.post78"
+version_tuple = (1, 0, 78)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post74")
+    pversion = V("1.0.post78")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.0.post21"
-data_version_tuple = (1, 0, 21)
+data_version_str = "1.0.post25"
+data_version_tuple = (1, 0, 25)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.0.post21")
+    pdata_version = V("1.0.post25")
 except ImportError:
     pass
-data_git_hash = "40b018af6667b54e5c4f05d4844148f4c04e09e7"
-data_git_describe = "v1.0-21-g40b018a"
+data_git_hash = "acbedbe9c4bbff4bb0d2cdda725a95f2e1c42adc"
+data_git_describe = "v1.0-25-gacbedbe"
 data_git_msg = """\
-commit 40b018af6667b54e5c4f05d4844148f4c04e09e7
+commit acbedbe9c4bbff4bb0d2cdda725a95f2e1c42adc
 Author: Olof Kindgren <olof.kindgren@gmail.com>
-Date:   Fri May 8 22:56:31 2020 +0200
+Date:   Tue May 26 22:52:09 2020 +0200
 
-    Align RISC-V compliance test target code with upstream changes
+    Prepare for release
 
 """
 
