34facab8d57f8ad838407a38d6b4c0c242da184f
LiteX Robot <robot@timvideos.us>
2021-02-04 04:07:31 +0000

    Updating pythondata-cpu-serv to 1.0.post173
    Updated data to v1.0-92-gbc9705b based on bc9705bef2a8e1f21b0df7f8a59cc8541851e811 from https://github.com/olofk/serv.
    > commit bc9705bef2a8e1f21b0df7f8a59cc8541851e811
    > Author: somhi <jordisx@gmail.com>
    > Date:   Sat Jan 30 12:52:38 2021 +0100
    >
    >     add support for SoCKit development kit board
    >
    
    Updated using 0.0.post81 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.0.post172"
-version_tuple = (1, 0, 172)
+version_str = "1.0.post173"
+version_tuple = (1, 0, 173)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post172")
+    pversion = V("1.0.post173")
 except ImportError:
     pass
 
@@ -32,11 +32,11 @@ Date:   Sat Jan 30 12:52:38 2021 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post80"
-tool_version_tuple = (0, 0, 80)
+tool_version_str = "0.0.post81"
+tool_version_tuple = (0, 0, 81)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post80")
+    ptool_version = V("0.0.post81")
 except ImportError:
     pass
 
