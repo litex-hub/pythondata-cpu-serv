ac3a8429b1957f00ff754eb0e067fc48c8b7aa2b
LiteX Robot <robot@timvideos.us>
2021-02-04 02:02:48 +0000

    Updating pythondata-cpu-serv to 1.0.post164
    Updated data to v1.0-92-gbc9705b based on bc9705bef2a8e1f21b0df7f8a59cc8541851e811 from https://github.com/olofk/serv.
    > commit bc9705bef2a8e1f21b0df7f8a59cc8541851e811
    > Author: somhi <jordisx@gmail.com>
    > Date:   Sat Jan 30 12:52:38 2021 +0100
    >
    >     add support for SoCKit development kit board
    >
    
    Updated using 0.0.post72 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,39 +4,39 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.0.post135"
-version_tuple = (1, 0, 135)
+version_str = "1.0.post164"
+version_tuple = (1, 0, 164)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post135")
+    pversion = V("1.0.post164")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.0.post69"
-data_version_tuple = (1, 0, 69)
+data_version_str = "1.0.post92"
+data_version_tuple = (1, 0, 92)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.0.post69")
+    pdata_version = V("1.0.post92")
 except ImportError:
     pass
-data_git_hash = "731ca8bb45f9182cd6d0ababcad3055a305fe298"
-data_git_describe = "v1.0-69-g731ca8b"
+data_git_hash = "bc9705bef2a8e1f21b0df7f8a59cc8541851e811"
+data_git_describe = "v1.0-92-gbc9705b"
 data_git_msg = """\
-commit 731ca8bb45f9182cd6d0ababcad3055a305fe298
-Author: Bruno Flores <bruno@brunoflores.com.br>
-Date:   Sat Oct 24 12:04:41 2020 +0000
+commit bc9705bef2a8e1f21b0df7f8a59cc8541851e811
+Author: somhi <jordisx@gmail.com>
+Date:   Sat Jan 30 12:52:38 2021 +0100
 
-    Allow for a configurable toolchain prefix.
+    add support for SoCKit development kit board
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post66"
-tool_version_tuple = (0, 0, 66)
+tool_version_str = "0.0.post72"
+tool_version_tuple = (0, 0, 72)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post66")
+    ptool_version = V("0.0.post72")
 except ImportError:
     pass
 
