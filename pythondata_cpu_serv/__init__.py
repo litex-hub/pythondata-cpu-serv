fa09909cc3f6cff36dfa30e3257b0ddf4e09035d
LiteX Robot <robot@timvideos.us>
2021-02-15 16:42:47 +0000

    Updating pythondata-cpu-serv to 1.0.post186
    Updated data to v1.0-96-g7624466 based on 7624466ddd552dbd8c94127149527638dee3ca37 from https://github.com/olofk/serv.
    > commit 7624466ddd552dbd8c94127149527638dee3ca37
    > Author: Olof Kindgren <olof.kindgren@gmail.com>
    > Date:   Sun Feb 14 22:13:49 2021 +0100
    >
    >     Optimize serv_rf_ram_if
    >
    
    Updated using 0.0.post90 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.0.post185"
-version_tuple = (1, 0, 185)
+version_str = "1.0.post186"
+version_tuple = (1, 0, 186)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post185")
+    pversion = V("1.0.post186")
 except ImportError:
     pass
 
@@ -32,11 +32,11 @@ Date:   Sun Feb 14 22:13:49 2021 +0100
 """
 
 # Tool version info
-tool_version_str = "0.0.post89"
-tool_version_tuple = (0, 0, 89)
+tool_version_str = "0.0.post90"
+tool_version_tuple = (0, 0, 90)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post89")
+    ptool_version = V("0.0.post90")
 except ImportError:
     pass
 
