042c4f9d3f6edd14229bf0bb5178d45d14ea34e3
LiteX Robot <robot@timvideos.us>
2021-03-17 20:14:48 +0000

    Updating pythondata-cpu-serv to 1.0.post194
    Updated data to v1.0-99-g9b84539 based on 9b84539bc0a72fb4c99a15cd894ffb62e3cc8e05 from https://github.com/olofk/serv.
    > commit 9b84539bc0a72fb4c99a15cd894ffb62e3cc8e05
    > Author: Olof Kindgren <olof.kindgren@gmail.com>
    > Date:   Wed Mar 17 21:14:41 2021 +0100
    >
    >     Add LibreCores badge
    >
    
    Updated using 0.0.post95 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.0.post193"
-version_tuple = (1, 0, 193)
+version_str = "1.0.post194"
+version_tuple = (1, 0, 194)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post193")
+    pversion = V("1.0.post194")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.0.post98"
-data_version_tuple = (1, 0, 98)
+data_version_str = "1.0.post99"
+data_version_tuple = (1, 0, 99)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.0.post98")
+    pdata_version = V("1.0.post99")
 except ImportError:
     pass
-data_git_hash = "548b7fbb41770b80810e011de438aedf1c814dd5"
-data_git_describe = "v1.0-98-g548b7fb"
+data_git_hash = "9b84539bc0a72fb4c99a15cd894ffb62e3cc8e05"
+data_git_describe = "v1.0-99-g9b84539"
 data_git_msg = """\
-commit 548b7fbb41770b80810e011de438aedf1c814dd5
+commit 9b84539bc0a72fb4c99a15cd894ffb62e3cc8e05
 Author: Olof Kindgren <olof.kindgren@gmail.com>
-Date:   Sun Mar 14 23:22:50 2021 +0100
+Date:   Wed Mar 17 21:14:41 2021 +0100
 
-    remove redundant ALU control signal
+    Add LibreCores badge
 
 """
 
