e1e23b811b3d4d8dc7e79b9a695f4dcbbd8b758a
LiteX Robot <robot@timvideos.us>
2021-12-23 22:03:42 +0000

    Updating pythondata-cpu-serv to 1.0.post265
    Updated data to v1.0-152-g28953fe based on 28953fec4c02314f17f78c5666915a43851995fe from https://github.com/olofk/serv.
    > commit 28953fec4c02314f17f78c5666915a43851995fe
    > Author: Olof Kindgren <olof.kindgren@gmail.com>
    > Date:   Fri Oct 8 22:42:02 2021 +0200
    >
    >     Simplify shift_op signal
    >
    
    Updated using 0.0.post113 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,39 +4,39 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.0.post260"
-version_tuple = (1, 0, 260)
+version_str = "1.0.post265"
+version_tuple = (1, 0, 265)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post260")
+    pversion = V("1.0.post265")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.0.post148"
-data_version_tuple = (1, 0, 148)
+data_version_str = "1.0.post152"
+data_version_tuple = (1, 0, 152)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.0.post148")
+    pdata_version = V("1.0.post152")
 except ImportError:
     pass
-data_git_hash = "99f82af6eb6b7fe5dad682bd315349fcd37fa102"
-data_git_describe = "v1.0-148-g99f82af"
+data_git_hash = "28953fec4c02314f17f78c5666915a43851995fe"
+data_git_describe = "v1.0-152-g28953fe"
 data_git_msg = """\
-commit 99f82af6eb6b7fe5dad682bd315349fcd37fa102
+commit 28953fec4c02314f17f78c5666915a43851995fe
 Author: Olof Kindgren <olof.kindgren@gmail.com>
-Date:   Sun Oct 3 23:28:45 2021 +0200
+Date:   Fri Oct 8 22:42:02 2021 +0200
 
-    Simplify optional MDU logic
+    Simplify shift_op signal
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post112"
-tool_version_tuple = (0, 0, 112)
+tool_version_str = "0.0.post113"
+tool_version_tuple = (0, 0, 113)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post112")
+    ptool_version = V("0.0.post113")
 except ImportError:
     pass
 
