86fa67cf6232f4ccfa2efe1a3fab4f553320599a
LiteX Robot <robot@timvideos.us>
2021-12-24 00:31:56 +0000

    Updating pythondata-cpu-serv to 1.1.0.post137
    Updated data to 1.1.0-20-g28953fe based on 28953fec4c02314f17f78c5666915a43851995fe from https://github.com/olofk/serv.
    > commit 28953fec4c02314f17f78c5666915a43851995fe
    > Author: Olof Kindgren <olof.kindgren@gmail.com>
    > Date:   Fri Oct 8 22:42:02 2021 +0200
    >
    >     Simplify shift_op signal
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,24 +4,24 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.0.post265"
-version_tuple = (1, 0, 265)
+version_str = "1.1.0.post137"
+version_tuple = (1, 1, 0, 137)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.post265")
+    pversion = V("1.1.0.post137")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.0.post152"
-data_version_tuple = (1, 0, 152)
+data_version_str = "1.1.0.post20"
+data_version_tuple = (1, 1, 0, 20)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.0.post152")
+    pdata_version = V("1.1.0.post20")
 except ImportError:
     pass
 data_git_hash = "28953fec4c02314f17f78c5666915a43851995fe"
-data_git_describe = "v1.0-152-g28953fe"
+data_git_describe = "1.1.0-20-g28953fe"
 data_git_msg = """\
 commit 28953fec4c02314f17f78c5666915a43851995fe
 Author: Olof Kindgren <olof.kindgren@gmail.com>
@@ -32,11 +32,11 @@ Date:   Fri Oct 8 22:42:02 2021 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post113"
-tool_version_tuple = (0, 0, 113)
+tool_version_str = "0.0.post117"
+tool_version_tuple = (0, 0, 117)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post113")
+    ptool_version = V("0.0.post117")
 except ImportError:
     pass
 
