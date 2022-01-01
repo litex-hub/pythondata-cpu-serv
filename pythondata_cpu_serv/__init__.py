c74ea7769cac3b44f3b0a1eb031966cb07f8774c
LiteX Robot <robot@timvideos.us>
2022-01-01 22:22:49 +0000

    Updating pythondata-cpu-serv to 1.1.0.post141
    Updated data to 1.1.0-24-gf04a510 based on f04a510393585f0a68280c912ae568b2dccc11cf from https://github.com/olofk/serv.
    > commit f04a510393585f0a68280c912ae568b2dccc11cf
    > Author: Olof Kindgren <olof.kindgren@gmail.com>
    > Date:   Sat Jan 1 22:43:43 2022 +0100
    >
    >     Remove unused parameter from serv_mem_if
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.1.0.post140"
-version_tuple = (1, 1, 0, 140)
+version_str = "1.1.0.post141"
+version_tuple = (1, 1, 0, 141)
 try:
     from packaging.version import Version as V
-    pversion = V("1.1.0.post140")
+    pversion = V("1.1.0.post141")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.1.0.post23"
-data_version_tuple = (1, 1, 0, 23)
+data_version_str = "1.1.0.post24"
+data_version_tuple = (1, 1, 0, 24)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.1.0.post23")
+    pdata_version = V("1.1.0.post24")
 except ImportError:
     pass
-data_git_hash = "07193819972aed16e8474913dbbbee5e01570e58"
-data_git_describe = "1.1.0-23-g0719381"
+data_git_hash = "f04a510393585f0a68280c912ae568b2dccc11cf"
+data_git_describe = "1.1.0-24-gf04a510"
 data_git_msg = """\
-commit 07193819972aed16e8474913dbbbee5e01570e58
+commit f04a510393585f0a68280c912ae568b2dccc11cf
 Author: Olof Kindgren <olof.kindgren@gmail.com>
-Date:   Sat Jan 1 17:15:00 2022 +0100
+Date:   Sat Jan 1 22:43:43 2022 +0100
 
-    Add ViDBo support
+    Remove unused parameter from serv_mem_if
 
 """
 
