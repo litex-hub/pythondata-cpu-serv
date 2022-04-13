1c964b91910d4bd16e35b9d030bf7f79eab35b17
LiteX Robot <robot@timvideos.us>
2022-04-13 06:34:30 +0000

    Updating pythondata-cpu-serv to 1.1.0.post160
    Updated data to 1.1.0-34-g2df5923 based on 2df592340f43a4a2951362960909863812bb8c1f from https://github.com/olofk/serv.
    > commit 2df592340f43a4a2951362960909863812bb8c1f
    > Author: Usman <32096123+usmnzain@users.noreply.github.com>
    > Date:   Wed Apr 13 10:48:58 2022 +0500
    >
    >     Compliance update
    >
    >     Updated serv to support latest version of riscv-arch-test (v2.6.1)
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,30 +4,32 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.1.0.post159"
-version_tuple = (1, 1, 0, 159)
+version_str = "1.1.0.post160"
+version_tuple = (1, 1, 0, 160)
 try:
     from packaging.version import Version as V
-    pversion = V("1.1.0.post159")
+    pversion = V("1.1.0.post160")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.1.0.post33"
-data_version_tuple = (1, 1, 0, 33)
+data_version_str = "1.1.0.post34"
+data_version_tuple = (1, 1, 0, 34)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.1.0.post33")
+    pdata_version = V("1.1.0.post34")
 except ImportError:
     pass
-data_git_hash = "7d08abb92d405b9416f9d7a77fdaba8ba0f52acb"
-data_git_describe = "1.1.0-33-g7d08abb"
+data_git_hash = "2df592340f43a4a2951362960909863812bb8c1f"
+data_git_describe = "1.1.0-34-g2df5923"
 data_git_msg = """\
-commit 7d08abb92d405b9416f9d7a77fdaba8ba0f52acb
-Author: Olof Kindgren <olof.kindgren@gmail.com>
-Date:   Wed Mar 9 20:59:53 2022 +0100
+commit 2df592340f43a4a2951362960909863812bb8c1f
+Author: Usman <32096123+usmnzain@users.noreply.github.com>
+Date:   Wed Apr 13 10:48:58 2022 +0500
 
-    Improve makehex.py
+    Compliance update
+    
+    Updated serv to support latest version of riscv-arch-test (v2.6.1)
 
 """
 
