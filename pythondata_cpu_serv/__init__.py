256a465d3012648d1d908236b832c41b217443cd
LiteX Robot <robot@timvideos.us>
2022-05-25 20:16:19 +0000

    Updating pythondata-cpu-serv to 1.1.0.post166
    Updated data to 1.1.0-35-g4ddd154 based on 4ddd154b249f46522f164aa9a49cce3edfafe03c from https://github.com/olofk/serv.
    > commit 4ddd154b249f46522f164aa9a49cce3edfafe03c
    > Author: Wadood <82087831+Abdulwadoodd@users.noreply.github.com>
    > Date:   Tue May 17 12:11:56 2022 +0500
    >
    >     Nexys 2 Board support
    >
    >     Added nexys 2 target support for servant
    >
    
    Updated using 0.0.post131 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.1.0.post163"
-version_tuple = (1, 1, 0, 163)
+version_str = "1.1.0.post166"
+version_tuple = (1, 1, 0, 166)
 try:
     from packaging.version import Version as V
-    pversion = V("1.1.0.post163")
+    pversion = V("1.1.0.post166")
 except ImportError:
     pass
 
@@ -34,11 +34,11 @@ Date:   Tue May 17 12:11:56 2022 +0500
 """
 
 # Tool version info
-tool_version_str = "0.0.post128"
-tool_version_tuple = (0, 0, 128)
+tool_version_str = "0.0.post131"
+tool_version_tuple = (0, 0, 131)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post128")
+    ptool_version = V("0.0.post131")
 except ImportError:
     pass
 
