351e0dd642acaa23edf958acf80f3cd94a724aec
LiteX Robot <robot@timvideos.us>
2022-06-13 11:10:41 +0000

    Updating pythondata-cpu-serv to 1.1.0.post180
    Updated data to 1.1.0-38-ga8f7de9 based on a8f7de9e8c57227fcd187a685193148094bd3fc0 from https://github.com/olofk/serv.
    > commit a8f7de9e8c57227fcd187a685193148094bd3fc0
    > Author: Abd <abdulwadood.afzal88@gmail.com>
    > Date:   Tue May 31 03:36:35 2022 +0500
    >
    >     compressed parameter added for Nexys-2 FPGA target
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.1.0.post178"
-version_tuple = (1, 1, 0, 178)
+version_str = "1.1.0.post180"
+version_tuple = (1, 1, 0, 180)
 try:
     from packaging.version import Version as V
-    pversion = V("1.1.0.post178")
+    pversion = V("1.1.0.post180")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.1.0.post36"
-data_version_tuple = (1, 1, 0, 36)
+data_version_str = "1.1.0.post38"
+data_version_tuple = (1, 1, 0, 38)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.1.0.post36")
+    pdata_version = V("1.1.0.post38")
 except ImportError:
     pass
-data_git_hash = "2655861447054d37b032795dcedc0fecf1d49dc3"
-data_git_describe = "1.1.0-36-g2655861"
+data_git_hash = "a8f7de9e8c57227fcd187a685193148094bd3fc0"
+data_git_describe = "1.1.0-38-ga8f7de9"
 data_git_msg = """\
-commit 2655861447054d37b032795dcedc0fecf1d49dc3
+commit a8f7de9e8c57227fcd187a685193148094bd3fc0
 Author: Abd <abdulwadood.afzal88@gmail.com>
-Date:   Wed Mar 23 15:21:58 2022 +0500
+Date:   Tue May 31 03:36:35 2022 +0500
 
-    Compressed Extension for SERV
+    compressed parameter added for Nexys-2 FPGA target
 
 """
 
