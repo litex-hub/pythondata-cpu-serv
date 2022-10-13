6344ab76f79cfcfc94bc7d89e6a126bcef803259
LiteX Robot <robot@timvideos.us>
2022-10-13 09:24:05 +0000

    Updating pythondata-cpu-serv to 1.2.0.post145
    Updated data to 1.2.0-3-g6ad60f6 based on 6ad60f69a20c969ce46a605af9661b224d7e765d from https://github.com/olofk/serv.
    > commit 6ad60f69a20c969ce46a605af9661b224d7e765d
    > Author: gojimmypi <13059545+gojimmypi@users.noreply.github.com>
    > Date:   Thu Dec 23 12:10:43 2021 -0800
    >
    >     Add ICE-V Wireless support
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_serv/__init__.py
+++ pythondata_cpu_serv/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/olofk/serv"
 
 # Module version
-version_str = "1.2.0.post143"
-version_tuple = (1, 2, 0, 143)
+version_str = "1.2.0.post145"
+version_tuple = (1, 2, 0, 145)
 try:
     from packaging.version import Version as V
-    pversion = V("1.2.0.post143")
+    pversion = V("1.2.0.post145")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.2.0.post1"
-data_version_tuple = (1, 2, 0, 1)
+data_version_str = "1.2.0.post3"
+data_version_tuple = (1, 2, 0, 3)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.2.0.post1")
+    pdata_version = V("1.2.0.post3")
 except ImportError:
     pass
-data_git_hash = "5cc7b0cbe19ae9b02e3c778b5d2f9cb75b9d9eb4"
-data_git_describe = "1.2.0-1-g5cc7b0c"
+data_git_hash = "6ad60f69a20c969ce46a605af9661b224d7e765d"
+data_git_describe = "1.2.0-3-g6ad60f6"
 data_git_msg = """\
-commit 5cc7b0cbe19ae9b02e3c778b5d2f9cb75b9d9eb4
-Author: Eric Brombaugh <ebrombaugh1@cox.net>
-Date:   Mon Aug 8 12:17:56 2022 -0700
+commit 6ad60f69a20c969ce46a605af9661b224d7e765d
+Author: gojimmypi <13059545+gojimmypi@users.noreply.github.com>
+Date:   Thu Dec 23 12:10:43 2021 -0800
 
-    Guarantee at least 2 cycles of o_rst after PLL locked.
+    Add ICE-V Wireless support
 
 """
 
